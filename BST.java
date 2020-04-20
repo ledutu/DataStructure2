6b423885e2a1fb6a50fef31f333114116bdbba2a
ledutu <javajava159@gmail.com>
2020-04-20 20:53:43 +0700

    asdfs

--- BST.java
+++ BST.java
@@ -1,19 +1,6 @@
 import java.lang.Integer;
 import java.lang.String;
 
-class Node {
-	Integer key;
-	Node left, right;
-	int size;
-	int height;
-
-	public Node(Integer key, int size, int height) {
-		this.key = key;
-		this.size = size;
-		this.height = height;
-	}
-}
-
 class BST {
 	Node x;
 
